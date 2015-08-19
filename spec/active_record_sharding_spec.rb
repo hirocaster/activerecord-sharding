95b428b98e781567d17838681d3fbfbdd3bc54e5
hirocaster <hohtsuka@gmail.com>
2015-08-19 10:35:49 +0900

    Delete fail spec

--- spec/active_record_sharding_spec.rb
+++ spec/active_record_sharding_spec.rb
@@ -4,8 +4,4 @@ describe ActiveRecordSharding do
   it 'has a version number' do
     expect(ActiveRecordSharding::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
