fcecb8ba235644319636a7abb96741cfdac3151f
hirocaster <hohtsuka@gmail.com>
2015-08-20 19:20:16 +0900

    Add spec, mixin module sequencer

---
+++ spec/active_record_sharding/sequencer_spec.rb
@@ -0,0 +1,31 @@
+describe ActiveRecordSharding::Model do
+  let!(:model) do
+    Class.new(ActiveRecord::Base) do
+      def self.name
+        'User'
+      end
+
+      include ActiveRecordSharding::Sequencer
+      use_sequencer :user
+    end
+  end
+
+  describe '#current_sequence_id' do
+    it 'returns current sequence id' do
+      expect(model.current_sequence_id).to be_a_kind_of Fixnum
+    end
+  end
+  describe '#next_sequence_id' do
+    let(:current_id) { model.current_sequence_id }
+    let(:next_id) { model.next_sequence_id }
+
+    it 'returns next sequence id' do
+      expect(current_id + 1).to eq next_id
+      expect(next_id).to be_a_kind_of Fixnum
+    end
+
+    it 'next sequence id > current sequence id' do
+      expect(current_id).to be < next_id
+    end
+  end
+end
