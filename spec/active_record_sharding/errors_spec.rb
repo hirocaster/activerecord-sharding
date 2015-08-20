8a2c8b34ea8d036cc1b04396993a559b2178f9d6
hirocaster <hohtsuka@gmail.com>
2015-08-21 00:56:22 +0900

    Fix typo

--- spec/active_record_sharding/errors_spec.rb
+++ spec/active_record_sharding/errors_spec.rb
@@ -3,7 +3,7 @@ describe ActiveRecordSharding::Error do
     expect(described_class).to be_a Class
   end
 
-  it 'kind of StandardEroor' do
+  it 'kind of StandardError' do
     expect(described_class.new).to be_kind_of StandardError
   end
 end
