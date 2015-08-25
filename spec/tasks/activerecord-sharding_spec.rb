2bd4e4799f2fe525387daee882c7d2af06ca0385
hirocaster <hohtsuka@gmail.com>
2015-08-25 17:13:28 +0900

    Add spec for tasks

--- spec/tasks/activerecord-sharding_spec.rb
+++ spec/tasks/activerecord-sharding_spec.rb
@@ -12,11 +12,63 @@ describe 'Tasks :active_record_sharding' do
     @rake[task].reenable
   end
 
-  describe 'info' do
-    let(:task) { 'active_record:sharding:info' }
+  context 'for sharding clusters' do
+    describe 'info' do
+      let(:task) { 'active_record:sharding:info' }
 
-    it 'show cluster infomation to STDOUT' do
-      expect { @rake[task].invoke }.to output(/test_user_001/).to_stdout
+      it 'show cluster infomation to STDOUT' do
+        expect { @rake[task].invoke }.to output(/test_user_001/).to_stdout
+      end
+    end
+
+    describe 'setup' do
+      let(:task) { 'active_record:sharding:setup' }
+
+      before do
+        allow(ActiveRecord::Tasks::DatabaseTasks).to receive(:create).and_return(nil)
+        allow(ActiveRecord::Tasks::DatabaseTasks).to receive(:load_schema).and_return(nil)
+      end
+
+      it 'Setup all cluster databases' do
+        expect(@rake[task].invoke[0]).to be_a Proc
+      end
+    end
+
+    describe 'drop_all' do
+      let(:task) { 'active_record:sharding:drop_all' }
+
+      before do
+        allow(ActiveRecord::Tasks::DatabaseTasks).to receive(:drop).and_return(nil)
+      end
+
+      it 'Drop all cluster databases' do
+        expect(@rake[task].invoke[0]).to be_a Proc
+      end
+    end
+  end
+  context 'for sequencers' do
+    describe 'setup' do
+      let(:task) { 'active_record:sharding:sequencer:setup' }
+
+      before do
+        allow(ActiveRecord::Sharding::DatabaseTasks).to receive(:execute).and_return(nil)
+      end
+
+      it 'Setup all sequencers databases' do
+        expect(@rake[task].invoke[0]).to be_a Proc
+      end
+    end
+
+    describe 'drop_all' do
+      let(:task) { 'active_record:sharding:sequencer:drop_all' }
+
+      before do
+        allow(ActiveRecord::Tasks::DatabaseTasks).to receive(:drop).and_return(nil)
+      end
+
+      it 'Drop all sequencers databases' do
+        expect(@rake[task].invoke[0]).to be_a Proc
+      end
     end
   end
 end
