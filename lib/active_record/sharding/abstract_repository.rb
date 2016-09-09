f0629d6f20b21037b97b7c01d9c27a269b362093
tpdn <tpdn@users.noreply.github.com>
2016-09-09 19:18:41 +0900

    Support marshalling and unmarshalling

--- lib/active_record/sharding/abstract_repository.rb
+++ lib/active_record/sharding/abstract_repository.rb
@@ -5,20 +5,21 @@ module ActiveRecord
 
         def generate_model_for_shard(connection_name)
           base_class_name = @base_class.name
-          class_name = generate_class_name connection_name
+          class_name      = generate_class_name connection_name
 
-          model = Class.new(base_class) do
+          return @base_class.const_get(class_name) if @base_class.const_defined?(class_name)
+
+          @base_class.const_set(class_name, Class.new(base_class) do
             self.table_name = base_class.table_name
 
             module_eval <<-RUBY, __FILE__, __LINE__ + 1
-                  def self.name
-                    "#{base_class_name}::#{class_name}"
-                  end
-                RUBY
-          end
+              def self.name
+                "#{base_class_name}::#{class_name}"
+              end
 
-          model.class_eval { establish_connection(connection_name) }
-          model
+              establish_connection(connection_name)
+            RUBY
+          end)
         end
 
         def generate_class_name(connection_name) # rubocop:disable Lint/UnusedMethodArgument
